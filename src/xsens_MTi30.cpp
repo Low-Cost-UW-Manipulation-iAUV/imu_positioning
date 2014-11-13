5fa287669dd038bfa737cc691492f96300a8cd88
Raphael Nagel <compiaffe@users.noreply.github.com>
2014-11-13 19:37:21 +0000

    changed frame_id from /odom to odom

--- src/xsens_MTi30.cpp
+++ src/xsens_MTi30.cpp
@@ -106,7 +106,7 @@ void xsens::imu_callback(const sensor_msgs::Imu::ConstPtr& message) {
         if (z_calibrated || y_calibrated || x_calibrated) {
             send_me.header.seq = message->header.seq;
             send_me.header.stamp = ros::Time::now();
-            send_me.header.frame_id = "/odom";
+            send_me.header.frame_id = "odom";
             send_me.twist.covariance.fill(0.0);
             send_me.twist.covariance[1] = calibration_x.get_variance();
             send_me.twist.covariance[7] = calibration_y.get_variance();
