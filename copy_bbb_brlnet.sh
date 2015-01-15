8f56b94c3eb6d2960f9c197e6b2396c9d2f46bfe
Raphael Nagel <compiaffe@users.noreply.github.com>
2015-01-15 19:20:42 +0000

    fix

--- copy_bbb_brlnet.sh
+++ copy_bbb_brlnet.sh
@@ -5,7 +5,7 @@ rsync -avzh ./src/*.cpp 						BBB_BRLNET:/home/ubuntu/uwesub_catkin_workspace/sr
 rsync -avzh ./include/imu_positioning/*.hpp 	BBB_BRLNET:/home/ubuntu/uwesub_catkin_workspace/src/imu_positioning/include/imu_positioning/
 rsync -avzh CMakeLists.txt 						BBB_BRLNET:/home/ubuntu/uwesub_catkin_workspace/src/imu_positioning/
 rsync -avzh *.xml 								BBB_BRLNET:/home/ubuntu/uwesub_catkin_workspace/src/imu_positioning/
-rsync -avzh *.launch	 						BBB_BRLNET:/home/ubuntu/uwesub_catkin_workspace/src/imu_positioning/
+rsync -avzh ./launch/*.launch	 						BBB_BRLNET:/home/ubuntu/uwesub_catkin_workspace/src/imu_positioning/launch/
 rsync -avzh *.yaml		 						BBB_BRLNET:/home/ubuntu/uwesub_catkin_workspace/src/imu_positioning/
 rsync -avzh *.md								BBB_BRLNET:/home/ubuntu/uwesub_catkin_workspace/src/imu_positioning/
 rsync -avzh ./urdf/*.urdf						BBB_BRLNET:/home/ubuntu/uwesub_catkin_workspace/src/imu_positioning/urdf/
--- copy_bmt.sh
+++ copy_bmt.sh
@@ -5,7 +5,7 @@ rsync -avzh ./src/*.cpp 						bmt:/home/devel/catkin_ws/src/imu_positioning/src/
 rsync -avzh ./include/imu_positioning/*.hpp 	bmt:/home/devel/catkin_ws/src/imu_positioning/include/imu_positioning/
 rsync -avzh CMakeLists.txt 						bmt:/home/devel/catkin_ws/src/imu_positioning/
 rsync -avzh *.xml 								bmt:/home/devel/catkin_ws/src/imu_positioning/
-rsync -avzh ./launch/*.launch	 				bmt:/home/devel/catkin_ws/src/imu_positioning/
+rsync -avzh ./launch/*.launch	 				bmt:/home/devel/catkin_ws/src/imu_positioning/launch/
 rsync -avzh *.yaml		 						bmt:/home/devel/catkin_ws/src/imu_positioning/
 rsync -avzh *.md								bmt:/home/devel/catkin_ws/src/imu_positioning/
 rsync -avzh ./urdf/*.urdf						bmt:/home/devel/catkin_ws/src/imu_positioning/urdf/
